e83dc7af7585deccbbbb64df3ff248deec45b3fa
Dan Loewenherz <dloewenherz@gmail.com>
2012-07-19 14:14:03 -0700

    set scroll timeout

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ function randomInteger() {
 }
 
 $(function() {
+    setTimeout(function() {
+        window.scrollTo(0, 1);
+    }, 1000);
+
     setInterval(function() {
         $('body').css("background", "rgb(" + randomInteger() + ", " + randomInteger() + ", " + randomInteger() + ")");
     }, 2000);
