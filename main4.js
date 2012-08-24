3b72bb60b4f1c1ea69693791042224e6caeb3ca0
Dan Loewenherz <dloewenherz@gmail.com>
2012-08-24 08:36:23 -0700

    update preloading

--- main4.js
+++ main4.js
@@ -8,8 +8,12 @@ $(function() {
         var width = elem.width();
         var height = elem.height();
         var pos = elem.position();
+        var src = "media/img/" + elem.attr("img");
+        img = new Image()
+        img.src = src;
+
         elem.hover(function() {
-            $("img#screenshot").attr("src", "media/img/" + elem.attr("img"));
+            $("img#screenshot").attr("src", src);
             container.removeClass("fadeout");
             container.addClass("fadein");
             container.css({left: pos.left + (width - 200) / 2 + "px", top: pos.top + height + 12 + "px" });
